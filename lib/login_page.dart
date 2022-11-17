a4cc48d6e7d4ece566736935022e0549911b1d91
주재호 <joo@jujaehoui-MacBookAir.local>
2022-11-17 16:35:55 +0900

    FIRST

--- lib/login_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoginPage extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Container(
-      child: Text('Login Page'),
-    );
-  }
-}
